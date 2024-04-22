9fe8c3e5272fd1e1a78f2c51ecf08d9feaace793
cr7st74n <cris-082011@hotmail.com>
2024-04-21 23:19:53 -0400

    new settings

--- src/ArrayQueue.java
+++ src/ArrayQueue.java
@@ -2,59 +2,68 @@
 // ArrayQueue.java
 // An array-based implementation of the classic FIFO queue interface.
 //**********************************************************
-public class ArrayQueue implements QueueADT
-{
-    private final int DEFAULT_SIZE = 5;
-    private Object[] elements;
-    private int numElements;
-    private int front, back;
-    //---------------------------------------------
-// Constructor; creates array of default size.
-//---------------------------------------------
-    public ArrayQueue()
-    {
+public class ArrayQueue implements QueueADT {
+    private Object[] queueArray;
+    private int front; // Index the front
+    private int back; // Index the back
+    private int nElements; // Number of elements
+
+    // Constructor
+    public ArrayQueue(int capacity) {
+        queueArray = new Object[capacity];
+        front = 0;
+        back = -1; // the queue is empty
+        nElements = 0;
     }
-    //---------------------------------------------
-// Puts item on end of queue.
-//---------------------------------------------
-    public void enqueue(Object item)
-    {
+
+    @Override
+    public void enqueue(Object item) {
+        if (!isFull()) {
+            back = (back + 1) % queueArray.length;
+            queueArray[back] = item;
+            nElements++;
+        } else {
+            System.out.println("Queue is full. Cannot enqueue.");
+        }
     }
-    //---------------------------------------------
-// Removes and returns object from front of queue.
-//---------------------------------------------
-    public Object dequeue()
-    {
+
+    @Override
+    public Object dequeue() {
+        if (!isEmpty()) {
+            Object dequeuedItem = queueArray[front];
+            queueArray[front] = null;
+            front = (front + 1) % queueArray.length;
+            nElements--;
+            return dequeuedItem;
+        } else {
+            System.out.println("Queue is empty. Cannot dequeue.");
+            return null;
+        }
     }
-    //---------------------------------------------
-// Returns true if queue is empty.
-//---------------------------------------------
-    public boolean isEmpty()
-    {
+
+    @Override
+    public boolean isEmpty() {
+        return nElements == 0;
     }
-    //-----------------------------------------
-// Returns true if queue is full.
-//-----------------------------------------
-    public boolean isFull()
-    {
+
+    @Override
+    public boolean isFull() {
+        return nElements == queueArray.length;
     }
-    //---------------------------------------------
-// Returns the number of elements in the queue.
-//---------------------------------------------
-    public int size()
-    {
+
+    @Override
+    public int size() {
+        return nElements;
     }
-    //---------------------------------------------
-// Returns a string containing the elements of the queue
-// from first to last
-//---------------------------------------------
-    public String toString()
-    {
-        String result = "\n";
-        for (int i = front, count=0; count < numElements;
-             i=(i+1)%elements.length,count++)
-            result = result + elements[i]+ "\n";
-        return result;
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < nElements; i++) {
+            int index = (front + i) % queueArray.length;
+            result.append(queueArray[index]).append("\n");
+        }
+        return result.toString();
     }
 }
-//**********************************************************
+
